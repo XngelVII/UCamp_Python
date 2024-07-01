91aa9c17d408247d5729f4d7cbed7de21709909d
M.T <mtapiahernandez@gmail.com>
2024-06-30 20:42:14 -0600

    agregando codigo para la calculadora de IMC a un nuevo repositorio

---
+++ hola_mundo.py
@@ -0,0 +1,18 @@
+
+# numero = 147 
+# type(numero)
+# print(type(numero))
+
+
+# numero = 14.7 
+# type(numero)
+# print(type(numero))
+
+
+variable = 0
+print (variable)
+print (type(variable))
+
+variable = bool(variable)
+print (variable)
+print (type(variable))
