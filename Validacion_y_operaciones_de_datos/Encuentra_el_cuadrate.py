3074b2feeea9c230169c911ce45193f1276f9154
M.T <mtapiahernandez@gmail.com>
2024-08-03 02:49:15 -0600

    Actualizados los script para la entrega del segundo proyecto.

--- Validacion_y_operaciones_de_datos/Encuentra_el_cuadrate.py
+++ Validacion_y_operaciones_de_datos/Encuentra_el_cuadrate.py
@@ -1,40 +1,44 @@
 # Mensaje de bienvenida al usuario
-print(f"Hola, te ayudaré a saber en qué cuadrante se encuentran las coordenadas que me indiques\n\nPara salir ingresa los valores x = 0.1 y y = 0.1")
+print(f"Hola, te ayudaré a saber en qué cuadrante se encuentran las coordenadas que me indiques\n")
 
-# while para que mientras "x" y "y" sean variables flotantes se ejecuten las condicionales.
-# Agregué un  en cada condicional ya que de otra manera el programa se iniciaba recursivamente luego de dar un primer resultado (pedia de nuevo valores para "x" y "y", daba un resultado y se reiniciaba)
+# while con opciones para que el usuario pueda escoger si indtroduce coordenadas o sale del programa
 while True:
     try:
+        opcion = input('Escoge una opcion:\n(1) Ingresar coordenadas.\n(2) Salir.\n')
+        if opcion != '1' and opcion != '2':
+            print('\nPor favor escoge una opcion correcta.\n')
+        elif opcion == '1':
         # Peticion al usuario de variables "x", "y" y asignacion a tipo flotante para que se acepten numeros con decimales
-        x = float(input("Por favor ingresa el valor de X: "))
-        y = float(input("Por favor ingresa el valor de Y: "))
-        # Condicional para determinar si las variables son de tipo flotante o no. Si son flotantes se ejecutaran las siguientes conficionales. Si no son flotantes se mandara mensaje de exepcion
-        if isinstance(x, float) and isinstance(y, float):
-            # Condicional para determinar si el punto esta en el origen cartesiano
-            if x == 0 and y == 0:
-                print (f"El Punto para las coordenadas que proporcionaste se encuentra en el Origen Cartesiano")
-            # Condicional para determinar si el punto esta en el eje y
-            elif x == 0: 
-                print (f"El Punto para las coordenadas que proporcionaste se encuentra en el Eje Y")
-            # Condicional para determinar si el punto esta en el eje x
-            elif y == 0:
-                print (f"El Punto para las coordenadas que proporcionaste se encuentra en el Eje X")
-            # Condicional para determinar si el punto esta en el cuadrante 1 con "x" y "y" ambas positivas
-            elif x > 0 and y > 0 and x != 0.1 and y != 0.1:
-                print (f"El Punto para las coordenadas que proporcionaste se encuentra en el Cuadrante 1")
-            # Condicional para determinar si el punto esta en el cuadrante 3 con "x" y "y" ambas negativas
-            elif x < 0 and y < 0:
-                print (f"El Punto para las coordenadas que proporcionaste se encuentra en el Cuadrante 3")
-            # Condicional para determinar si el punto esta en el cuadrante 2 con "x" positiva y "y" negativa
-            elif x > 0 and y < 0 and x != 0.1 :
-                print (f"El Punto para las coordenadas que proporcionaste se encuentra en el Cuadrante 2")
-            # Condicional para determinar si el punto esta en el cuadrante 2 con "x" negativa y "y" positiva
-            elif x < 0 and y > 0 and y != x != 0.1:
-                print (f"El Punto para las coordenadas que proporcionaste se encuentra en el Cuadrante 4")  
-            elif x == 0.1 and y == 0.1:
-                print(f"\n\nGracias por usar este programa.\n\nPor cierto esos valores estan en el Cuadrante 1.\n\nHasta pronto.")
-                exit()
-            else:
-                print(f"Algo salió mal u_u.")
+            x = float(input("Por favor ingresa el valor de X: "))
+            y = float(input("Por favor ingresa el valor de Y: "))
+            # Condicional para determinar si las variables son de tipo flotante o no. Si son flotantes se ejecutaran las siguientes conficionales. Si no son flotantes se mandara mensaje de exepcion
+            if isinstance(x, float) and isinstance(y, float):
+                # Condicional para determinar si el punto esta en el origen cartesiano
+                if x == 0 and y == 0:
+                    print (f"El Punto para las coordenadas que proporcionaste se encuentra en el Origen Cartesiano.\n")
+                # Condicional para determinar si el punto esta en el eje y
+                elif x == 0: 
+                    print (f"El Punto para las coordenadas que proporcionaste se encuentra en el Eje Y.\n")
+                # Condicional para determinar si el punto esta en el eje x
+                elif y == 0:
+                    print (f"El Punto para las coordenadas que proporcionaste se encuentra en el Eje X.\n")
+                # Condicional para determinar si el punto esta en el cuadrante 1 con "x" y "y" ambas positivas
+                elif x > 0 and y > 0:
+                    print (f"El Punto para las coordenadas que proporcionaste se encuentra en el Cuadrante 1.\n")
+                # Condicional para determinar si el punto esta en el cuadrante 3 con "x" y "y" ambas negativas
+                elif x < 0 and y < 0:
+                    print (f"El Punto para las coordenadas que proporcionaste se encuentra en el Cuadrante 3.\n")
+                # Condicional para determinar si el punto esta en el cuadrante 2 con "x" positiva y "y" negativa
+                elif x > 0 and y < 0:
+                    print (f"El Punto para las coordenadas que proporcionaste se encuentra en el Cuadrante 2.\n")
+                # Condicional para determinar si el punto esta en el cuadrante 2 con "x" negativa y "y" positiva
+                elif x < 0 and y > 0:
+                    print (f"El Punto para las coordenadas que proporcionaste se encuentra en el Cuadrante 4.\n")
+                else:
+                    print(f"Algo salió mal u_u.\n")
+        elif opcion == '2':
+            # Mensaje de salida
+            print(f"\nGracias por usar este programa.\n\nHasta pronto.\n")
+            exit()
     except ValueError:
-        print(f"Por favor ingresa un números. No uses letras o símbolos.")
+        print('Algo sallio mal uwu\n')
--- Validacion_y_operaciones_de_datos/Longitud_de_una_frase.py
+++ Validacion_y_operaciones_de_datos/Longitud_de_una_frase.py
@@ -1,25 +1,32 @@
-print(f"\nHola, este es un programa que te dice cuantas letras tiene la palabra que le escribas.\n\nSi quieres salir escribe la palabra exit.\n\n")
-
+# Mensaje de saludo para el Usuario asi como instruccion para salir del programa.
+print(f"\nHola, este es un programa que te dice cuantas letras tiene la palabra que escribas.\n\nSi quieres salir escribe la palabra exit.\n")
+# While para que se el programa se ejecute recursivamente.
 while True:
     try:
+        # Mensaje para que el usuario ingrese una palabra.
         palabra = input(str("Por favor introduce una palabra:"))
+        # if para identificar las palabras con la longitud correcta (4 - 8 letras) diferente de la palabra para salir 'exit'.
         if 3 < len(palabra) < 9 and palabra != "exit":
             print(f"\nLa palabra es {palabra} y tiene {len(palabra)} letras. Su longitud es correcta.\n")
+        # elif para palabras menores a 4 letras con sus propias condicionales para si falta 1 letra o mas letras.
         elif len(palabra) < 4:
             faltan = 4 - len(palabra) 
             if faltan == 1:
-                print(f"\nLa palabra {palabra} solo tiene {len(palabra)} letras. \nFalta {faltan} letra.\n")                
+                print(f"\nLa palabra {palabra.upper()} solo tiene {len(palabra)} letras. \nFalta {faltan} letra.\n")                
             else:
-                print(f"\nLa palabra {palabra} solo tiene {len(palabra)} letras. \nFaltan {faltan} letras.\n")             
+                print(f"\nLa palabra {palabra.upper()} solo tiene {len(palabra)} letras. \nFaltan {faltan} letras.\n")             
+        # elif para palabras mayores a 8 letras con sus propias condicionales para si sobre 1 letra o mas letras.
         elif len(palabra) > 8:
             sobran = len(palabra) - 8            
             if sobran == 1:
-                print(f"\nLa palabra {palabra} tiene {len(palabra)} letras. \nSobra {sobran} letra.\n")                
+                print(f"\nLa palabra {palabra.upper()} tiene {len(palabra)} letras. \nSobra {sobran} letra.\n")                
             else:
-                print(f"\nLa palabra {palabra} tiene {len(palabra)} letras. \nSobran {sobran} letras.\n")
+                print(f"\nLa palabra {palabra.upper()} tiene {len(palabra)} letras. \nSobran {sobran} letras.\n")
+        # elif para si el usuario introduce la palabra de salida se imprima un mensaje de salida.        
         elif palabra == "exit":
             print(f"Gracias por usar este programa. Hasta luego.\n")
             exit()
+        # else por si llega a malir sal algo.
         else:
             print(f"Algo salió mal UwU.")
     except:
